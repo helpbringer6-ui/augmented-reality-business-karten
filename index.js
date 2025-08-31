0fd84abc5525642e76db9717058f02fa1001bf78
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-08-31 03:53:43 -0500

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server läuft auf http://localhost:${PORT}`);
+});
