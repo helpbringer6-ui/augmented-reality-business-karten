16518100ffad5da2df5f3e2660feeec8decade64
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-08-31 03:57:39 -0500

    Add comments for better code understanding

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ const PORT = process.env.PORT || 3000;
 // Middleware for parsing JSON bodies
 app.use(bodyParser.json());
 // Serve static files from the 'public' directory
-app.use(express.static('public'));
+app.use(express.static('public'));  
 
 // Route for the home page
 app.get('/', (req, res) => {
+  // Send the index.html file in response to root requests
   res.sendFile(__dirname + '/public/index.html');
 });
 
